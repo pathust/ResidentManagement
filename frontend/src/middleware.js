edefbe2d5ba5cf5c5d7805e109eb141eb3772a37
Nguyễn Ngọc Khánh <67950963+kjhave@users.noreply.github.com>
2025-10-25 14:00:05 +0700

    fix: route protector xóa cookies

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,13 +1,14 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const accessToken = request.cookies.get("access_token");
+  	const accessToken = request.cookies.get("access_token");
 	const permissions = request.cookies.get("permissions")?.value;
 	const { pathname } = request.nextUrl;
 	if (!accessToken || !permissions){
-		request.cookies.delete("access_token");
-		request.cookies.delete("permissions");
-		return NextResponse.redirect(new URL("/login", request.url));
+		const response = NextResponse.redirect(new URL("/login", request.url))
+		response.cookies.delete("access_token");
+		response.cookies.delete("permissions");
+		return response;
 	}
 
 	if (pathname.startsWith("/dashboard/nhankhau") && !permissions.includes("PERSON_MANAGE")) {
