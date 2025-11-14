875204fcd05983b0685cd8aa3b1e543d08671116
Nguyễn Ngọc Khánh <67950963+kjhave@users.noreply.github.com>
2025-11-15 05:40:23 +0700

    chore: household api

---
+++ frontend/src/app/api/household/route.js
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+
+// Handle GET requests to fetch persons data
+export async function GET(req) {
+    const SERVER_URL = process.env.SERVER_URL || "http://localhost:8080";
+
+    if (!req.cookies.get("access_token")) {
+        return NextResponse.json({ error: "No access token found" }, { status: 401 });
+    }
+
+    const token = req.cookies.get("access_token")?.value || "";
+
+    if (!req.cookies.get("permissions")) {
+        return NextResponse.json({ error: "No permissions found" }, { status: 401 });
+    }
+
+    const queryParams = req.nextUrl.search.toString() || "";
+    
+    try {
+        const response = await fetch(`${SERVER_URL}/api/households${queryParams}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`,
+            },
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            return NextResponse.json({ error: errorData.detail || "Failed to get household data" }, { status: response.status });
+        }
+
+        const data = await response.json();
+        
+        return NextResponse.json(data);
+    }	catch (error) {
+        console.error("Error during fetch:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
