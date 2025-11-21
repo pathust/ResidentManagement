3755f6fdd920fb0e6e754aea1731324a173da8d4
Nguyễn Ngọc Khánh <67950963+kjhave@users.noreply.github.com>
2025-11-22 04:59:33 +0700

    feat: households's api

--- frontend/src/app/dashboard/hokhau/page.js
+++ frontend/src/app/dashboard/hokhau/page.js
@@ -24,7 +24,6 @@ export default function HoKhauPage() {
     try {
       const data = await householdAPI.getAll();
       setHouseholdData(data);
-      console.log("Fetched household data:", data);
     } catch (error) {
       console.error(error);
       message.error("Không thể tải danh sách hộ khẩu");
