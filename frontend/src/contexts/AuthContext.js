5a3a6f7449e535ab9e2fcb0df1bcaec2c6e9e167
Nguyễn Ngọc Khánh <67950963+kjhave@users.noreply.github.com>
2025-12-07 20:24:01 +0700

    feat: util

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -66,7 +66,7 @@ export function AuthProvider({ children }) {
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout, loading }}>
+    <AuthContext.Provider value={{ user, login, logout, loading, util }}>
       {children}
     </AuthContext.Provider>
   );
